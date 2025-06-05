6d20a04e9094013b548e80241fcd9d91b50e09b7
Jangbe <jangebe91@gmail.com>
2025-06-05 22:56:44 +0700

    change pwa package

--- fb/next.config.ts
+++ fb/next.config.ts
@@ -1,60 +1,13 @@
-import type { NextConfig } from "next";
-import {
-  PHASE_DEVELOPMENT_SERVER
-} from "next/constants.js";
-import { RuntimeCaching } from 'workbox-build';
-import { runtimeCaching } from "@ducanh2912/next-pwa";
+import withSerwistInit from "@serwist/next";
 
-const nextConfig: NextConfig = {
-  // config lainnya
-};
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+  cacheOnNavigation: true,
+});
 
-const nextConfigFunction = async (phase: string) => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-
-  const customRuntimeCaching: RuntimeCaching[] = [
-    ...runtimeCaching,
-    {
-      urlPattern: /^\/list(\/.*)?$/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'batik-detail-pages',
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24 * 7,
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-    {
-      urlPattern: /^\/api\/batik(\/.*)?$/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'batik-api-detail',
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24 * 7,
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-  ];
-
-  const withPWA = (await import("@ducanh2912/next-pwa")).default({
-    dest: "public",
-    disable: isDev,
-    register: true,
-    workboxOptions: {
-      skipWaiting: true,
-      runtimeCaching: customRuntimeCaching,
-    },
-  });
-
-  return withPWA(nextConfig);
-};
-
-export default nextConfigFunction;
+export default withSerwist({
+  // Your Next.js config
+});
