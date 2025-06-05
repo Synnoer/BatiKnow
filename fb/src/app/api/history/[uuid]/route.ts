35492853e136b7c853ac84596bc0d9c3f1c748d4
Jangbe <jangebe91@gmail.com>
2025-06-05 08:14:35 +0700

    fix: bug in detail history

--- fb/src/app/api/history/[uuid]/route.ts
+++ fb/src/app/api/history/[uuid]/route.ts
@@ -3,12 +3,12 @@ import { User } from '@/context/AuthContext';
 import { verifyToken } from '@/lib/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest, { params }: { params: { uuid: string } }) {
+export async function GET(req: NextRequest, { params }: { params: Promise<{ uuid: string }> }) {
   const token = req.cookies.get('token');
   if (!token?.value) return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
 
   const user = verifyToken(token.value) as User;
-  const { uuid } = params;
+  const { uuid } = await params;
 
   const history = await prisma.history.findFirst({
     where: {
